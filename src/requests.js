ae25cd1652f40f8695dfbe8d0f8e6a1da22e3c23
AlbertoWindows <albertopimentel04@gmail.com>
2020-08-16 18:42:49 -0500

    first commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "0aa2360519f89bc8a2d3f70470809894";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&languaje=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fecthTopRated: `/movie/top_rated?apo_key=${API_KEY}&languaje=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
